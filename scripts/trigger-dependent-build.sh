7cec608d5399488d7ee6be24b5d02ea18cf41982
Rebecca Law <rebecca.law@digital.cabinet-office.gov.uk>
2015-11-20 10:36:46 +0000

    Change name for functional test build

--- scripts/trigger-dependent-build.sh
+++ scripts/trigger-dependent-build.sh
@@ -13,4 +13,4 @@
 #   travis token
 #
 
-curl -vvv -s -X POST -H "Content-Type: application/json" -H "Accept: application/json" -H "Travis-API-Version: 3" -H "Authorization: token $auth_token" -d '{"request":{"branch":"master"}}' https://api.travis-ci.org/repo/alphagov%2Fnotify-functional-tests/requests
+curl -vvv -s -X POST -H "Content-Type: application/json" -H "Accept: application/json" -H "Travis-API-Version: 3" -H "Authorization: token $auth_token" -d '{"request":{"branch":"master"}}' https://api.travis-ci.org/repo/alphagov%2Fnotifications-functional-tests/requests
