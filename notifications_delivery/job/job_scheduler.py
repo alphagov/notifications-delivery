9817c38cabbea38c688fa7cbbb21e92d728567e3
Nicholas Staples <nicholas.staples@solution-tribute.com>
2016-02-08 16:50:49 +0000

    scheduler job added.

--- notifications_delivery/job/job_scheduler.py
+++ notifications_delivery/job/job_scheduler.py
@@ -1,6 +1,7 @@
 from apscheduler.schedulers.background import BackgroundScheduler
 
 from notifications_delivery.job.jobs import process_jobs
+from notifications_delivery.processor.sqs_processor import process_notification_job
 
 
 class JobScheduler(object):
@@ -8,6 +9,7 @@ class JobScheduler(object):
     def __init__(self, interval_seconds=60):
         self.scheduler = BackgroundScheduler()
         self.scheduler.add_job(process_jobs, 'interval', seconds=interval_seconds, max_instances=1)
+        self.scheduler.add_job(process_notification_job, 'interval', seconds=interval_seconds, max_instances=1)
 
     def start(self):
         self.scheduler.start()
